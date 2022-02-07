a3e196b5580db000194d2c0e61fd4c42a209b7f9
ayeshasiddiqaSystemsltd <90602395+ayeshasiddiqaSystemsltd@users.noreply.github.com>
2022-02-07 14:27:47 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,1 +1,18 @@
-
+#!/bin/bash
+if [[ -f 'file01.txt' ]];
+then 
+  cp file01.txt $WORKSPACE/AyeshaSiddiqa
+else
+    if [[ -f 'file02.txt' ]];
+    then 
+      cp file03.txt $WORKSPACE/AyeshaSiddiqa 
+    else
+        if  [[ -f 'file03.txt' ]];
+        then 
+          cp file03.txt $WORKSPACE/AyeshaSiddiqa
+        else
+          echo "Files Doesn't exist"
+        fi
+     fi
+fi
+         
