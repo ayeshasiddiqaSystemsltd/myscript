c1402d7d4ad46b4e9403b7bb6d8d698637961698
ayeshasiddiqaSystemsltd <90602395+ayeshasiddiqaSystemsltd@users.noreply.github.com>
2022-02-07 18:31:17 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,26 +2,26 @@
 if [[ -f 'FileA.txt' ]];
 then
   cp -f FileA.txt /home/systemsltd/
-if [[ -f 'file01.txt' ]];
-then
-  echo "Copying file01"
-  cp -f file01.txt /home/systemsltd/
-else
-  echo "File Doesn't exist"
-fi
-if [[ -f 'file02.txt' ]];
-then 
-  echo "Copying file02"
-  cp -f file02.txt /home/systemsltd/
-else
-  echo "Files Doesn't exist"
-fi
-if  [[ -f 'file03.txt' ]];
-then
-  echo "Copying file03"
-  cp -f file03.txt /home/systemsltd/
-else
-  echo "Files Doesn't exist"
+  if [[ -f 'file01.txt' ]];
+  then
+    echo "Copying file01"
+    cp -f file01.txt /home/systemsltd/
+  else
+    echo "File Doesn't exist"
+  fi
+  if [[ -f 'file02.txt' ]];
+  then 
+    echo "Copying file02"
+    cp -f file02.txt /home/systemsltd/
+  else
+    echo "Files Doesn't exist"
+  fi
+  if  [[ -f 'file03.txt' ]];
+  then
+    echo "Copying file03"
+    cp -f file03.txt /home/systemsltd/
+  else
+    echo "Files Doesn't exist"
 else
   echo "FileA not exist"
 fi
