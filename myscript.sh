4052d845cba0f77938159e60d356181f91b0c355
ayeshasiddiqaSystemsltd <90602395+ayeshasiddiqaSystemsltd@users.noreply.github.com>
2022-02-07 14:36:27 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,21 +2,21 @@
 if [[ -f 'file01.txt' ]];
 then 
   echo "Copying file01"
-  cp file01.txt $WORKSPACE/AyeshaSiddiqa
+  cp file01.txt $WORKSPACE/AyeshaSiddiqa/myfiles
 else
   echo "File Doesn't exist"
 fi
 if [[ -f 'file02.txt' ]];
 then
   echo "Copying file02"
-  cp file02.txt $WORKSPACE/AyeshaSiddiqa
+  cp file02.txt $WORKSPACE/AyeshaSiddiqa/myfiles
 else
   echo "Files Doesn't exist"
 fi
 if  [[ -f 'file03.txt' ]];
 then
   echo "Copying file03"
-  cp file03.txt $WORKSPACE/AyeshaSiddiqa
+  cp file03.txt $WORKSPACE/AyeshaSiddiqa/myfiles
 else
   echo "Files Doesn't exist"
 fi
