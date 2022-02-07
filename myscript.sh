99df1634e35cd16d01c1155b1ca30ec41d366298
ayeshasiddiqaSystemsltd <90602395+ayeshasiddiqaSystemsltd@users.noreply.github.com>
2022-02-07 15:42:26 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -10,13 +10,17 @@ else
   echo "File Doesn't exist"
 fi
 if [[ -f 'file02.txt' ]];
-then
+then 
+  chmod 777 file01.txt
+  ls -lrt
   echo "Copying file02"
   cp file02.txt /home/systemsltd/Ayesha/
   echo "Files Doesn't exist"
 fi
 if  [[ -f 'file03.txt' ]];
 then
+  chmod 777 file01.txt
+  ls -lrt
   echo "Copying file03"
   cp file03.txt /home/systemsltd/Ayesha/
 else
