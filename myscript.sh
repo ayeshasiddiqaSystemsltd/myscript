60315758bc726068d41f3791a8c56644e034b972
ayeshasiddiqaSystemsltd <90602395+ayeshasiddiqaSystemsltd@users.noreply.github.com>
2022-02-07 14:29:49 +0500

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 if [[ -f 'file01.txt' ]];
 then 
+  echo "Copying file01"
   cp file01.txt $WORKSPACE/AyeshaSiddiqa
 else
     if [[ -f 'file02.txt' ]];
-    then 
-      cp file03.txt $WORKSPACE/AyeshaSiddiqa 
+    then
+      echo "Copying file02"
+      cp file02.txt $WORKSPACE/AyeshaSiddiqa 
     else
         if  [[ -f 'file03.txt' ]];
-        then 
+        then
+          echo "Copying file03"
           cp file03.txt $WORKSPACE/AyeshaSiddiqa
         else
           echo "Files Doesn't exist"
